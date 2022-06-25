d384b85ed5207696f4364241171f9de29323817d
Nigel Y <nigelyyz@yahoo.com.sg>
2022-06-25 22:13:50 +0800

    Completed Education and Header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header>
+			<a 
+			href="https://github.com/beast88/cv-builder" 
+			target="_blank"
+			rel="noopener noreferrer"
+			><i className="fa fa-github"></i></a>
+
+			<h1 className="title">C.V. Builder</h1>
+			
+			<i className="fa fa-address-card"></i>
+		</header>
+	)
+
+}
+
+export default Header
